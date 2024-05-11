4e837c05e8b00299a22951a64dfb8a1faadd3d2f
Paul <paullocksley@outlook.com>
2024-05-11 15:21:25 +1000

    ai?

---
+++ FS/Views/AccountInformationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS.Views;
+
+public partial class AccountInformationView : ContentPage
+{
+    public AccountInformationView()
+    {
+        InitializeComponent();
+    }
+}
