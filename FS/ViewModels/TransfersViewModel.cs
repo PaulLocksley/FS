6256a53e24d51c52c3ae1904b92bc9d5d9ceada8
Paul <paullocksley@outlook.com>
2024-05-12 23:18:14 +1000

    loading and such.
    view tomorrow.

--- FS/ViewModels/TransfersViewModel.cs
+++ FS/ViewModels/TransfersViewModel.cs
@@ -1,19 +1,28 @@
 ﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FS.Models;
 
 namespace FS.ViewModels;
 
-public class TransfersViewModel
+
+public partial class TransfersViewModel : ObservableObject
 {
     public FileSenderServer FSServer;
+    
+    [ObservableProperty]
+    public Transfer[] transfers = [];
     public async Task ReloadConfig()
     {
-        var k = await FSServer.GetAllTransfers();
-        Debug.WriteLine(k.Length);
-        return;
+        Transfers = await FSServer.GetAllTransfers();
     }
 
     public TransfersViewModel(FileSenderServer fsServer)
     {
         FSServer = fsServer;
+    }
+    public TransfersViewModel()
+    {
+        FSServer = new FileSenderServer(new FileSenderServerConfig("",
+            "","",23,"",232323,1,12,1,1,new List<string>()));
     }
 }
